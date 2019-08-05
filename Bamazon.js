5f2b8f48f81506fbf4fb244485244c8d0905b9ee
ShivamSukhwal <shivam33@gmail.com>
2019-08-05 13:28:22 -0400

    shopping selection made

--- Bamazon.js
+++ Bamazon.js
@@ -4,10 +4,65 @@ var table = require("cli-table2");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  user: "",
-  password: "",
+  user: "root",
+  password: "root",
   database: "bamazon_db",
   port: 3306
 });
 
 connection.connect();
+
+var display = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("");
+    console.log("      Bamazon - Bam It's yours    ");
+    console.log("");
+    console.log("We Hope You Find What Your Looking For :)");
+    console.log("");
+    var tables = new table({
+      head: ["Id", "Product", "Price($)"],
+      colWidths: [10, 40, 10],
+      colAligns: ["center", "center", "right"],
+      style: {
+        head: ["lime"],
+        compact: true
+      }
+    });
+    for (var i = 0; i < res.length; i++) {
+      tables.push([res[i].id, res[i].product_name, res[i].Price]);
+    }
+
+    console.log(tables.toString());
+    console.log("");
+    shop();
+  });
+};
+
+var shop = function() {
+  inquirer
+    .prompt({
+      type: "input",
+      name: "desiredProduct",
+      message: "Please enter the ID of the prouct you would like to purchase."
+    })
+    .then(function(productId) {
+      var selection = productId.desiredProduct;
+      connection.query("SELECT * FROM products WHERE Id=?", selection, function(
+        err,
+        res
+      ) {
+        if (err) throw err;
+        if (res.length === 0) {
+          console.log(
+            "That Product doesn't exist, Please enter a Product Id from the list above"
+          );
+
+          shop();
+        } else {
+          console.log("Thanks for the selection");
+        }
+      });
+    });
+};
+display();
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE products
     (40) NOT NULL,
     department_id INT
     (50) NOT NULL,
-    price DECIMAL
+    prices DECIMAL
     (10,2) NOT NULL,
     quantity_available INT
     (10) NOT NULL,
@@ -29,12 +29,12 @@ VALUES
     (2, "Drill", 2, 49.99, 170),
     (3, "Dinosaur Toy", 3, 4.50, 120),
     (4, "Cricket Bat", 4, 99.99, 150),
-    (5, "Bamazon Bindle", 5, 145.99, 2000),
+    (5, "Bamazon Bindle", 5, 45.99, 2000),
     (6, "Flower Pot", 1, 4.99, 240),
     (7, "Nails", 2, 0.20, 25),
     (8, "Toy Guitar", 3, 25.99, 202),
     (9, "Volleyball", 4, 15.00, 100),
-    (10, "Bamazon Bill", 5, 199.99, 2000),
+    (10, "Bamazon Bill", 5, 99.99, 2000),
     (11, "Bamazon Water stick", 5, 25.99, 2000),
     (12, "Wickets", 4, 30.99, 4, 100),
     (13, "Plant Seeds", 1, 0.99, 1, 100);
